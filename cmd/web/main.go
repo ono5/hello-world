3b0d20f8f51b3878c6eaae11deb6db67c6db87bc
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-25 07:25:40 +0900

    feat: Added App Config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"helloworld/pkg/handlers"
+	"log"
 	"net/http"
+
+	"github.com/ono5/hello-world/pkg/config"
+	"github.com/ono5/helloworld/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template")
+	}
+
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
