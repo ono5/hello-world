63e9bf8fc0e10d1c5cb8a9eab815576cb1047a7b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-24 11:07:15 +0900

    Initial Commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// About is the home page handler
+func About(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Printf("Starting appllication on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, niil)
+}
