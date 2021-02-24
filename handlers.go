0819c0189da9efa4d045a4faccbad2f13f80b9a8
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-24 11:30:36 +0900

    refactor: Sepalate file2

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Home is the home page handler
@@ -14,14 +12,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the home page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	// Load the assigned template file
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsingg template:", err)
-		return
-	}
 }
