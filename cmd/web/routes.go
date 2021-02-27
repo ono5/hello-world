a9758fb406df9acfbd97689aba6fcc0cdc5c203a
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-27 16:41:13 +0900

    using chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"net/http"
-
 	"helloworld/pkg/config"
 	"helloworld/pkg/handlers"
+	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
-func routes(app *config.AppConfig) *pat.PatternServeMux {
-	mux := pat.New()
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
 
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module helloworld
 
 go 1.15
 
-require github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
+require (
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
+	github.com/go-chi/chi v1.5.3
+)
