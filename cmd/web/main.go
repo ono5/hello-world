9c7c28927c929cc2a4077baae43c85c294a381b9
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-28 16:35:40 +0900

    feat: Installing and setting up a sessions package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"helloworld/pkg/config"
 	"helloworld/pkg/handlers"
 	"helloworld/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module helloworld
 go 1.15
 
 require (
-	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
+	github.com/alexedwards/scs/v2 v2.4.0
 	github.com/go-chi/chi v1.5.3
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/justinas/nosurf v1.1.1
 )
