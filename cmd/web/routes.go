671d5324378bc6f55bbb7760e02702011642bc46
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-28 16:03:36 +0900

    feat: Developing our own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(middleware.Logger)
+	// mux.Use(middleware.Logger)
+	// mux.Use(writeToConsole)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
